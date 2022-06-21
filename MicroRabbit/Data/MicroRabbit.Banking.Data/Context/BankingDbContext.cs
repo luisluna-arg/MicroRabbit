22e18c87cc04b51d4d461874eb7f310ae30ec163
Luis Luna <luismluna87@gmail.com>
2022-06-21 03:48:08 -0300

    Partially completed Bankinh Microservice

---
+++ MicroRabbit/Data/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
